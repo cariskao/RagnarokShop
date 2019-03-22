9846890a4483b9249d5bc65ae62c627e5c4caa28
Ray <loveyou520babytw@gmail.com>
2019-03-22 11:34:10 +0800

    create project

--- src/bus.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue';
-
-Vue.prototype.$bus = new Vue();
-Vue.prototype.$cartBus = new Vue();
--- src/components/pages/breadcrumb.vue
+++
@@ -1,7 +0,0 @@
-<template lang="pug">
-  nav(aria-label='breadcrumb')
-    ol.breadcrumb.bg-transparent
-      li.breadcrumb-item
-        a(href='#') 首頁
-      li.breadcrumb-item.active(aria-current='page') 全部商品
-</template>
